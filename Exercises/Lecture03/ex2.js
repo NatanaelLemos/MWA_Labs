e32dc974e8ffb069282a2e0972b63cf6ce21c04f
Natanael Lemos <natanael.lemos@live.com>
2018-04-05 10:04:23 -0500

    Lecture03_ex2_question

--- Exercises/Lecture03/ex2.js
+++ Exercises/Lecture03/ex2.js
@@ -9,7 +9,15 @@
         res.writeHead(200, {
             'Content-Type': 'image/jpg'
         });
-
+        /*
+            Create a web server that's going to send a response of big 
+            image (bigger then 3MB) to any client that sends a request to your 
+            specified server:port. Use the best way for performance. 
+            (Try to solve this in many different ways and inspect the loading time in 
+            the browser and send many requests to see the performance 
+            differences) 
+        */
+       
         /*
             Artillery report @ 19:36:18(-0500) 2018-04-04
             Scenarios launched:  20
